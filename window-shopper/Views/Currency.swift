4d2c10dadda527b058cd9ddd6a3fa3ba27129bc4
Kat Reinhart <kat.reinhart@gmail.com>
2017-09-23 20:00:42 -0700

    Implement currency display and finalize app

--- window-shopper/Views/Currency.swift
+++ window-shopper/Views/Currency.swift
@@ -10,6 +10,24 @@ import UIKit
 
 @IBDesignable class Currency: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: (frame.size.height - size) / 2, y: (frame.size.height / 2 - size / 2), width: size, height: size))
+        
+        currencyLabel.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.7829555458)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.locale = .current
+        
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+        
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -27,6 +45,8 @@ import UIKit
         layer.cornerRadius = 5.0
         textAlignment = .center
         
+        clipsToBounds = true
+        
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
             
@@ -34,13 +54,4 @@ import UIKit
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
